c627c993122a585420307aceda85157a3b16854d
Mason Wilie <masonwilie@gmail.com>
2024-01-14 20:42:18 -0700

    More unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -116,3 +116,103 @@ TEST(push_back, string_rvalue)
     ASSERT_NO_THROW(v.at(0));
     EXPECT_EQ(v.at(0), "value");
 }
+
+TEST(emplace_back, empty_class_no_arguments)
+{
+    sv::stable_vector<EmptyClass> v;
+
+    v.emplace_back();
+
+    EXPECT_EQ(v.size(), 1);
+    EXPECT_GT(v.capacity(), 0);
+    
+    ASSERT_NO_THROW(v.at(0));
+}
+
+TEST(emplace_back, string_no_arguments)
+{
+    sv::stable_vector<std::string> v;
+
+    v.emplace_back();
+
+    EXPECT_EQ(v.size(), 1);
+    EXPECT_GT(v.capacity(), 0);
+    
+    ASSERT_NO_THROW(v.at(0));
+
+    EXPECT_EQ(v.at(0), "");
+}
+
+namespace
+{
+
+class ClassWithArguments
+{
+ public:
+    ClassWithArguments(int a, std::string b, double c) noexcept
+        : _a(a), _b(b), _c(c)
+    {}
+
+    bool operator==(const ClassWithArguments& other) const
+    {
+        return _a == other._a
+            && _b == other._b
+            && _c == other._c;
+    }
+
+ private:
+    const int _a;
+    const std::string _b;
+    const double _c;
+};
+
+}  // namespace
+
+TEST(emplace_back, class_with_arguments)
+{
+    sv::stable_vector<ClassWithArguments> v;
+
+    v.emplace_back(1, "test", 5.0);
+
+    EXPECT_EQ(v.size(), 1);
+    EXPECT_GT(v.capacity(), 0);
+    
+    ASSERT_NO_THROW(v.at(0));
+
+    EXPECT_EQ(v.at(0), ClassWithArguments(1, "test", 5.0));
+}
+
+TEST(front_back, int)
+{
+    sv::stable_vector<int> v;
+
+    v.emplace_back(1);
+    v.emplace_back(2);
+    v.emplace_back(3);
+
+    EXPECT_EQ(v.size(), 3);
+    EXPECT_GE(v.capacity(), 3);
+
+    auto& front = v.front();
+    auto& back = v.back();
+
+    EXPECT_EQ(front, 1);
+    EXPECT_EQ(back, 3);
+
+    front = 5;
+    back = 6;
+
+    EXPECT_EQ(v.front(), 5);
+    EXPECT_EQ(v.back(), 6);   
+}
+
+TEST(at, throws_exception)
+{
+    sv::stable_vector<int> v;
+    EXPECT_THROW(v.at(0), std::out_of_range);
+
+    v.push_back(1);
+
+    EXPECT_NO_THROW(v.at(0));
+    EXPECT_THROW(v.at(1), std::out_of_range);
+}
